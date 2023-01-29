3d0f39ad8efb19e0aae66a3982242d11774f4644
linux369 <shikhar@iiitmanipur.ac.in>
2023-01-29 08:12:50 +0530

    contract functions

--- warp/contact.js
+++ warp/contact.js
@@ -1,35 +1,35 @@
-// export function handle(state, action) {
-//     // create 
-//     // update
-//     // delete
+export function handle(state, action) {
+    // create 
+    // update
+    // delete
 
-//     if (action.input.function === 'initialize') {
-//         state.author = action.caller
-//     }
+    if (action.input.function === 'initialize') {
+        state.author = action.caller
+    }
 
-//     // we are only doing state update when caller is same as author
-//     if (action.input.function === 'createPost' && action.caller === state.author) {
-//         const posts = state.posts
-//         posts[action.input.post.id] = action.input.post
-//         state.posts = posts
-//     }
-//     // posts = {"1": {// post data} }
+    // we are only doing state update when caller is same as author
+    if (action.input.function === 'createPost' && action.caller === state.author) {
+        const posts = state.posts
+        posts[action.input.post.id] = action.input.post
+        state.posts = posts
+    }
+    // posts = {"1": {// post data} }
 
-//     if (action.input.function === 'updatePost' && action.caller === state.author) {
-//         const posts = state.posts // get ref to the post 
-//         const postToUpdate = action.input.post // ref to the post that are coming in
-//         posts[postToUpdate.id] = postToUpdate // updating post id that are coming in
-//         state.posts = posts // resetting the state
-//     }
+    if (action.input.function === 'updatePost' && action.caller === state.author) {
+        const posts = state.posts // get ref to the post 
+        const postToUpdate = action.input.post // ref to the post that are coming in
+        posts[postToUpdate.id] = postToUpdate // updating post id that are coming in
+        state.posts = posts // resetting the state
+    }
 
-//     if (action.input.function === 'delete Post' && action.caller === state.author) {
-//         const posts = state.posts
-//         delete posts[action.input.post.id]
-//         state.posts = posts
-//     }
+    if (action.input.function === 'delete Post' && action.caller === state.author) {
+        const posts = state.posts
+        delete posts[action.input.post.id]
+        state.posts = posts
+    }
 
 
-//     return {
-//         state
-//     }
-// }
+    return {
+        state
+    }
+}
